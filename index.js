34e9834a45c8aa7dacd7cd4904b8d9c954b3ebb9
UmberBn <humberto-bandeira@hotmail.com>
2021-10-25 19:46:03 -0400

    fixing socket connection

--- index.js
+++ index.js
@@ -3,29 +3,21 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors')
 
-app.use(bodyParser.json());
-
-
 const PORT = process.env.PORT || 3000;
-
 const postsController = require('./controller/posts')
-
-
 const http = require('http').createServer(app);
+app.use(bodyParser.json());
 app.use(cors());
-
 
 const io = require('socket.io')(http, {
   cors: {
-    origin: 'http://localhost:3000', 
+    origin: 'http://localhost:3001', 
     methods: ['GET', 'POST'], 
   }
 });
 
 app.post('/posts', postsController.create)
-app.get('/', postsController.getAll )
+app.get('/', postsController.getAll)
 require('./socket/posts')(io);
 
-
-
-app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
+http.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
--- socket/posts.js
+++ socket/posts.js
@@ -2,7 +2,7 @@ const { getAll, create } = require("../model/posts");
 
 module.exports = (io) =>
   io.on('connection', (socket) => {
-    socket.on('increasePosts', async(name, message) => {
+    socket.on('increasePosts', async({name, message}) => {
       await create(name, message)
       const posts = await getAll()
       io.emit('refreshPosts', posts )
